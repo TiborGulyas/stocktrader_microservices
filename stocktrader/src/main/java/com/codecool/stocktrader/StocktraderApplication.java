c0b4c7d18f1dedacc45743277b245be32addafc2
TiborGulyas <gulyastibor@gmail.com>
2021-02-09 17:06:02 +0100

    service-registry implemented

--- stocktrader/src/main/java/com/codecool/stocktrader/StocktraderApplication.java
+++ stocktrader/src/main/java/com/codecool/stocktrader/StocktraderApplication.java
@@ -5,11 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
+@EnableEurekaClient
 public class StocktraderApplication {
 
 	@Autowired
@@ -26,6 +34,15 @@ public class StocktraderApplication {
 			System.out.println("init persistance");
 			dataInitializer.initData();
 		};
+	}
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.ant("/**"))
+				.build();
 	}
 
 }
