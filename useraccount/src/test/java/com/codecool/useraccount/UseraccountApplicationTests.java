97727e23e0414e4236f0d3ebb9e4852ec2e0d26e
TiborGulyas <gulyastibor@gmail.com>
2021-02-10 14:01:23 +0100

    useraccount module created

---
+++ useraccount/src/test/java/com/codecool/useraccount/UseraccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.useraccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseraccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
